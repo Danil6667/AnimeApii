35e537a2dbdb7f92615660daa46b3c0ed8151589
Danil6667 <gnilidanil@gmail.com>
2023-03-28 16:11:34 +0600

    Initial commit

---
+++ app/src/main/java/com/example/animeapi/di/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.example.animeapi.di
+
+import com.example.data.remote.RetrofitClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideApiService() = RetrofitClient()
+
+    @Singleton
+    @Provides
+    fun provideAnimeApiService(retrofitClient: RetrofitClient) =
+        retrofitClient.provideAnimeApiService()
+
+    @Singleton
+    @Provides
+    fun provideMangaApiService(retrofitClient: RetrofitClient) =
+        retrofitClient.provideMangaApiService()
+}
